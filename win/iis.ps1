974f5a84fb0fc37d81bb22cd23f0415156060f3c
k.gonsovsky@gmail.com <k.gonsovsky@gmail.com>
2024-03-24 00:46:41 +0300

    new

--- win/iis.ps1
+++ win/iis.ps1
@@ -85,12 +85,11 @@ function CreateCertificate {
     $expiryDate = (Get-Date).AddYears(25)
     
     $path = Join-Path -Path $scriptDir -ChildPath "..\cert\$domain.cer"
+    $pathPfx = Join-Path -Path $scriptDir -ChildPath "..\cert\$domain.pfx"
 
     if (-not (Test-Path $path)) {
         Write-Host "Certificate creating... $path"
-
-        $pathPfx = Join-Path -Path $scriptDir -ChildPath "..\cert\$domain.pfx"
-
+  
         $cert = New-SelfSignedCertificate -DnsName $domain -CertStoreLocation "cert:\LocalMachine\My" -KeySpec KeyExchange -NotAfter $expiryDate -Subject "CN=$domain" -KeyExportPolicy Exportable -FriendlyName $friendlyName
     
         Move-Item -Path "Cert:\LocalMachine\My\$($cert.Thumbprint)" -Destination "Cert:\LocalMachine\Root" -Force:$force
@@ -100,7 +99,11 @@ function CreateCertificate {
     
         Export-Certificate -Cert $cert -FilePath $path -Force
     } else {
-        Write-Host "Certificate exists. $path"
+        Write-Host "Certificate exists. $pathPfx"
+        $certificatePassword = ConvertTo-SecureString -String "123" -Force -AsPlainText
+        $certificate = Import-PfxCertificate -FilePath $pathPfx -CertStoreLocation Cert:\LocalMachine\My -Password $certificatePassword -Exportable
+        $certificate = Import-PfxCertificate -FilePath $pathPfx -CertStoreLocation Cert:\LocalMachine\Root -Password $certificatePassword -Exportable
+        $certificate | Out-Null
     }
 }
 
